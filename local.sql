77e6b571ae7809d8d2b58d26fe47220183c6da58
kamdibus <kam.dibus@gmail.com>
2017-04-18 11:44:25 +0200

    Export not working. Including Worksheet dump

--- local.sql
+++ local.sql
@@ -1,7 +1,32 @@
-create table employee(
-pesel int primary key not null check ( pesel > 11111111111 and pesel < 99999999999),
-gender varchar(6),
-address varchar(30),
-salary int check (salary > 2000),
-bonus int check ( bonus > 100 and bonus < 20000)
-) 
+select * from name_gender;
+
+insert into employee values(
+'strasse', 2200, 1, 'Kamil', 2100); %eror
+
+alter table employee
+drop column pesel;
+
+alter table employee
+drop column gender;
+
+create table device(
+device_id int not null primary key,
+type varchar(20),
+owner int,
+foreign key (owner) references employee(employee_id) on delete set null)
+
+alter table name_gender
+add (constraint name_gender_pk_const primary key (name));
+
+alter table employee
+add (name varchar(20) )
+add (constraint name_gender_const foreign key (name) references name_gender(name));
+
+insert into name_gender values (
+'Kamil', 'male');
+insert into name_gender values (
+'Tomasz', 'male');
+insert into name_gender values (
+'Dominik', 'male');
+insert into name_gender values (
+'Ewa', 'female');
