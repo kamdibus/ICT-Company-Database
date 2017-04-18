285630ae84c251f86dc8e5aafa819de4201bf318
tomeleoon <tomasz.nowak.stw@gmail.com>
2017-04-18 22:53:54 +0200

    created all tables, todo - bad constraints

--- local.sql
+++ local.sql
@@ -1,32 +1,68 @@
+create table names_gender(
+first_name varchar2(8) not null primary key,
+gender char(7)
+);
+
 create table employees(
-emp_id integer primary key,
+employee_id integer primary key,
 pesel number(11,0) not null,
-name varchar2(8),
+first_name varchar2(8),
+foreign key (first_name) references names_gender(first_name),
 last_name varchar2(8),
 salary number(6,0) check (salary > 1000),
 address varchar2(20)
 );
 
-create table name_gender(
-name varchar2(8) not null primary key,
-gender char(7)
+create table managers(
+promotion_date date,
+bonus number(6,0)
 );
+
+alter table managers /* relation Manager ISA Employee */
+add (employee_id int not null references employees(employee_id))
+add (constraint manager_isa_pk primary key (employee_id));
 
 create table devices(
 device_id int not null primary key,
-type varchar(20),
-owner int,
-foreign key (owner) references employee(employee_id) on delete set null
+device_name varchar(20),
+owner int, /* relation Employee OWNS Device */
+foreign key (owner) references employees(employee_id) on delete set null
 );
 
-alter table employees
-add (constraint name_gender_const foreign key (name) references name_gender(name));
+create table teams(
+team_id integer not null primary key,
+team_name varchar2(20),
+manager_id int not null, /* relation Manager MANAGES Team */
+foreign key(manager_id) references managers(employee_id)
+);
 
-insert into name_gender values (
+create table clients(
+client_id int not null primary key,
+client_name varchar2(20)
+);
+
+create table projects(
+project_id int not null primary key,
+project_name varchar2(20),
+team_id int not null, /* relation Team CONTROLS Project */
+foreign key(team_id) references teams(team_id),
+client_id int not null, /* relation Client ORDERED Project */
+foreign key(client_id) references clients(client_id)
+);
+
+create table works_on( /* relation N:M WORKS_ON */
+employee_id int not null,
+foreign key (employee_id) references employees(employee_id) on delete cascade,
+project_id int not null,
+foreign key (project_id) references projects(project_id) on delete cascade,
+hours int
+);
+
+insert into names_gender values (
 'Kamil', 'male');
-insert into name_gender values (
+insert into names_gender values (
 'Tomasz', 'male');
-insert into name_gender values (
+insert into names_gender values (
 'Dominik', 'male');
-insert into name_gender values (
+insert into names_gender values (
 'Ewa', 'female');
