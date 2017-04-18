8e8891da57594885b99c807de7b2db7795badbbd
tomeleoon <tomasz.nowak.stw@gmail.com>
2017-04-18 23:32:52 +0200

    script to delete everything

---
+++ drop_all.sql
@@ -0,0 +1,10 @@
+/* run this script a few times, because there are foreign keys to delete */
+drop table clients;
+drop table projects;
+drop table works_on;
+drop table teams;
+drop table manages;
+drop table managers;
+drop table devices;
+drop table employees;
+drop table names_gender;
