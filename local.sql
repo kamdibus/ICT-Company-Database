378b1d875bb0db1473cfbc4e991b6d0603f098db
tomeleoon <tomasz.nowak.stw@gmail.com>
2017-04-26 13:27:11 +0200

    corrected errors with characters encoding and with procedure

--- local.sql
+++ local.sql
@@ -102,9 +102,9 @@ insert into names_gender values ('Tomasz', 'male');
 insert into names_gender values ('Anna', 'female');
 
 insert into employees values (seq_employees.nextval, 96123100001, 'Tomasz', 'Nowak', 4100, 'Warszawa...');
-insert into employees values (seq_employees.nextval, 96123100002, 'Kamil', 'BiduĹ›', 4100, 'Warszawa...');
-insert into employees values (seq_employees.nextval, 96123100003, 'Anna', 'Kowalska', 4200, 'KrakĂłw...');
-insert into employees values (seq_employees.nextval, 96123100004, 'Tomasz', 'Kowalski', 4000, 'KrakĂłw...');
+insert into employees values (seq_employees.nextval, 96123100002, 'Kamil', 'Biduś', 4100, 'Warszawa...');
+insert into employees values (seq_employees.nextval, 96123100003, 'Anna', 'Kowalska', 4200, 'Kraków...');
+insert into employees values (seq_employees.nextval, 96123100004, 'Tomasz', 'Kowalski', 4000, 'Kraków...');
 
 insert into clients values (seq_clients.nextval, 'klient 1 a');
 insert into clients values (seq_clients.nextval, 'klient 2 b');
@@ -159,7 +159,7 @@ insert into works_on select employees.employee_id, projects.project_id, 15
 from employees, projects where last_name='Nowak' and project_name='projekt 1';
 
 insert into works_on select employees.employee_id, projects.project_id, 11
-from employees, projects where last_name='BiduĹ›' and project_name='projekt 1';
+from employees, projects where last_name='Biduś' and project_name='projekt 1';
 
 insert into works_on select employees.employee_id, projects.project_id, 11
 from employees, projects where last_name='Kowalska' and project_name='projekt 2';
@@ -178,18 +178,18 @@ update managers set bonus = bonus + 200;
 update employees set salary = salary + 100;
 commit;
 end;
+/
 
 /* procedure - 0.5p */
-create or replace function change_salary(emp_id in int, amount in int)
-return employees.salary%type is
+create or replace procedure change_salary(emp_id in int, amount in int) is
 new_salary employees.salary%type;
 begin
   select salary into new_salary from employees where employee_id = emp_id;
+  new_salary := new_salary + amount;
   if new_salary < 0 then
     raise_application_error (-20001, 'WRONG DATA');
   end if;
   update employees set salary = new_salary where employee_id = emp_id;
-  return new_salary;
   exception
   when no_data_found then
     raise_application_error (-20001, 'WRONG DATA');
